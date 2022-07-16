49c6687b5e86bb243a98ee1f954f5b84f483ce7c
Marc Castro Trullá <marccastro02@gmail.com>
2022-07-16 20:12:24 +0200

    JavaScript: Array con bucle, arrays multidimensionales, clave-valor objetos...

---
+++ Algoritmos de JavaScript y Estructuras de Datos/Estructuras de datos básicas/exercicio12_crear_arrays_multidimensionales.js
@@ -0,0 +1,24 @@
+// Crea arreglos complejos multidimensionales
+
+let myNestedArray = [ // Nivel 1
+    // Cambia solo el código debajo de esta línea
+    // Nivel 2
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    ['concat', false, true, 'spread', 'array'],
+    ['mutate', 1327.98, 'splice', 'slice', 'push'],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth'],
+    [
+        //  Nivel 3
+        ["deep"],
+        [
+            // Nivel 4
+            ["deeper"],
+            [
+                // Nivel 5
+                ["deepest"]
+          ]
+        ]
+      ]
+    // Cambia solo el código encima de esta línea
+];
---
+++ Algoritmos de JavaScript y Estructuras de Datos/Estructuras de datos básicas/exercicio15_acceder_nombres_propiedad_notacion_corchetes.js
@@ -0,0 +1,18 @@
+// Accede a los nombres de propiedad con la notación de corchetes
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+function checkInventory(scannedItem) {
+    // Cambia solo el código debajo de esta línea
+    return foods[scannedItem];
+    // Cambia solo el código encima de esta línea
+}
+  
+console.log(checkInventory("apples"));
